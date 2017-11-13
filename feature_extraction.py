1d9d637da60132c105bc71a81347207175118335
Shae Brown <shaeb@uvic.ca>
2017-11-12 19:40:38 -0800

    Add more features, training and export of model

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,9 @@
 import librosa
 import numpy as np
 import sys
+from scipy import signal
+from scipy import stats
+from sklearn import preprocessing
 from onset_detection import OnsetDetect
 
 
@@ -27,20 +30,99 @@ class FeatureExtraction:
         self.features.append(spectral_centroid)
         return self
 
+    def with_rms(self):
+        rms = []
+        for onset in self.audio_clips:
+            r = librosa.feature.rmse(y=onset, frame_length=len(onset))
+            rms.append(r[0])
+        self.features.append(rms)
+        return self
+
+    def with_rms_of_filter(self, wp, ws, gpass, gstop):
+        n, wn = signal.buttord(wp, ws, gpass, gstop)
+        b, a = signal.butter(n, wn[0])
+        rms = []
+        for onset in self.audio_clips:
+            y = signal.lfilter(b, a, onset)
+            r = librosa.feature.rmse(y=y, frame_length=len(onset))
+            rms.append(r[0])
+        self.features.append(rms)
+        return self
+
+    def with_crest_factor(self):
+        crest = []
+        for onset in self.audio_clips:
+            max_abs = np.max(np.abs(onset))
+            r = librosa.feature.rmse(y=onset, frame_length=len(onset))
+            crest.append(max_abs/r[0])
+        self.features.append(crest)
+        return self
+
+    def with_spectral_kurtosis(self):
+        kurtosis = []
+        for onset in self.audio_clips:
+            ps = np.abs(np.fft.fft(onset)) ** 2
+            k = stats.kurtosis(ps, fisher=True)
+            kurtosis.append(k)
+        self.features.append(kurtosis)
+        return self
+
+    def with_spectral_skewness(self):
+        skewness = []
+        for onset in self.audio_clips:
+            ps = np.abs(np.fft.fft(onset)) ** 2
+            s = stats.skew(ps)
+            skewness.append(s)
+        self.features.append(skewness)
+        return self
+
+    def with_spectral_rolloff(self):
+        rolloff = []
+        for onset in self.audio_clips:
+            roll = librosa.feature.spectral_rolloff(y=onset, sr=self.sr, hop_length=len(onset))
+            rolloff.append(roll[0][0])
+        self.features.append(rolloff)
+        return self
+
+    def with_spectral_flatness(self):
+        flatness = []
+        for onset in self.audio_clips:
+            ps = np.abs(np.fft.fft(onset)) ** 2
+            f = stats.gmean(ps)/np.mean(ps)
+            flatness.append(f)
+        self.features.append(flatness)
+        return self
+
+    def with_mfcc(self):
+        mfcc = []
+        for onset in self.audio_clips:
+            c = librosa.feature.mfcc(y=onset, sr=self.sr, n_mfcc=1)
+            mfcc.append(c[0])
+        self.features.append(mfcc)
+        return self
+
     def get_feature_matrix(self):
-        # TODO: normalize each row in a consistent way
-        # transpose so each row is a onset time, and each column is a feature
+        norm = preprocessing.normalize(self.features, axis=0)
         return np.transpose(np.array(self.features))
 
 if __name__ == "__main__":
     infile = sys.argv[1]
     audio, sample_rate = librosa.load(infile)
     onset_clips = OnsetDetect(audio, sample_rate).get_onset_clips(0.001)
+    nyq = sample_rate/2
     X = FeatureExtraction(onset_clips, sample_rate)\
-        .with_spectral_centroid().\
-        with_zero_crossing_rate().\
-        get_feature_matrix()
-
+        .with_spectral_centroid()\
+        .with_zero_crossing_rate()\
+        .with_rms()\
+        .with_rms_of_filter(np.divide([49, 50], nyq), np.divide([0.01, 2000], nyq), 0.01, 62)\
+        .with_rms_of_filter(np.divide([200, 201], nyq), np.divide([1, 1300], nyq), 0.01, 20)\
+        .with_rms_of_filter(np.divide([5100, 16300], nyq), np.divide([65, 22000],nyq), 0.05, 60)\
+        .with_spectral_kurtosis()\
+        .with_spectral_skewness()\
+        .with_spectral_rolloff()\
+        .with_spectral_flatness()\
+        .with_mfcc()\
+        .get_feature_matrix()
     print(X)
 
 
--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,25 @@
 from flask import Flask
-from flask import jsonify
 from flask import render_template
 import re
+
 app = Flask(__name__)
+
+classes = {"Bass drum", "Bongo", "Chinese ride cymbal",
+           "Clave", "Cowbell", "Crash cymbal", "Crash cymbal 2",
+           "Cross stick", "Hi-hat closed", "Hi-hat open", "Low tom",
+           "Lowest tom", "Mid tom", "Ride cymbal", "Shaker", "Snare drum",
+           "Snare drum brush", "Snare rim shot", "Splash cymbal", "Tambourine", "Timbala", "Timbala rim shot"}
 
 @app.route('/testing')
 def get_test():
-	path = "./static/test_data/fort_minor-remember_the_name_127-245/"
-	drum_types = {"Snare drum", "Bass drum"}
-	drum_events = {}
-	for drum in drum_types:
-		with open(path + drum + ".txt", "r") as annotation:
-			times = []
-			for line in annotation:
-				times.append(float(re.findall("\d+\.\d+", line)[0]))
-			drum_events[drum] = times
-			annotation.close()
-	return render_template("music.html", drum_events=drum_events)
+    path = "./static/test_data/fort_minor-remember_the_name_127-145/"
+    drum_types = {"Snare drum", "Bass drum"}
+    drum_events = {}
+    for drum in drum_types:
+        with open(path + drum + ".txt", "r") as annotation:
+            times = []
+            for line in annotation:
+                times.append(float(re.findall("\d+\.\d+", line)[0]))
+            drum_events[drum] = times
+            annotation.close()
+    return render_template("music.html", drum_events=drum_events)
